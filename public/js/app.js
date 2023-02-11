e221d05e6868ce78cd7778560eabca009d60b86e
Otta25 <juanmanuelottado@gmail.com>
2023-02-11 18:17:31 -0300

    favicon agregado

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,6 @@ let articles = document.querySelectorAll('.article-card');
 
 
 articles.forEach(e => e.addEventListener('click',(e)=>{
- let titleValue = e.target.innerText
+ let titleValue = e.target.innerText;
  window.location.href =`http://localhost:3000/${titleValue}`
 }))
